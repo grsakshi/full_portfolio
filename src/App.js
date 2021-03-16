f20585eb3d2aabbac403d8ec06b65cdc4cdd0b31
grsakshi <sakshi.g642001@gmail.com>
2021-03-16 16:46:58 +0530

    Adding hero component and footer component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Navbar from './components/Navbar/navbar.component';
+import Hero from './components/Hero/hero.component';
+import Footer from './components/Footer/footer.component';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <Hero />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Nav_links/nav_links.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavLinks = () => (
+    <div>
+        <a className="px-4 md:px-8 lg:px-10 hover:text-blue-500 block sm:inline-block" href="#">
+          Home
+        </a>
+        <a className="px-4 md:px-8 lg:px-10 hover:text-blue-500 block sm:inline-block" href="#">
+          Features
+        </a>
+        <a className="px-4 md:px-8 lg:px-10 hover:text-blue-500 block sm:inline-block" href="#">
+          About
+        </a>
+        <a className="px-4 md:px-8 lg:px-10 hover:text-blue-500 block sm:inline-block" href="#">
+          Contact
+        </a>
+      </div>
+);
+
+export default NavLinks;
