0cc6c101365bb08be54714505dd20d3febacf222
grsakshi <sakshi.g642001@gmail.com>
2021-03-17 15:02:18 +0530

    adding arrow symbol to blog section link part

--- src/components/Blog/blog.component.jsx
+++ src/components/Blog/blog.component.jsx
@@ -7,7 +7,7 @@ const Blog = props => (
         <div className='relative h-52 w-52 text-gray-200 text-left p-2'>
             <span className='text-xxs'>{props.date}</span>
             <h3 className=' text-2xl leading-8 font-bold font-body text-gray-50 pb-2'>{props.title}</h3>
-            <a href={props.link} className='absolute bottom-4 left-6 text-sm text-blue-600 hover:text-blue-400 font-bold'>Read More -></a>
+            <a href={props.link} className='absolute bottom-4 left-6 text-sm text-blue-600 hover:text-blue-400 font-bold'>Read More &#8594;</a>
         </div>
     </div>
 );
